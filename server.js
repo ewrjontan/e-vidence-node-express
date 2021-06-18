5d871947f1acc307e695a298fcb6d007ff22d6b3
Jonathan Cardenas <jonscardenas@gmail.com>
2021-06-18 12:51:31 -0500

    added server and router file

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const morgan = require('morgan');
+
+//added for part 2
+const campsiteRouter = require('./routes/campsiteRouter');
+
+
+const hostname = 'localhost';
+const port = 3000;
+
+//server name is app
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+
+//added for part 2
+app.use('/campsites', campsiteRouter);
+
+/* moved to campsiteRouter
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});*/
+
+/*deleted for part 2
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});*/
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+//creates instance of http server class and listens to it
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
